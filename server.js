b24ec174db93fc335ed2ed25566d01e48c5f977c
PRANEEL GHOSH <ghoshpraneel@gmail.com>
2026-04-10 23:59:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 🔥 Routes
+app.use("/api", require("./routes/healthRoutes"));
+
+// 🚀 Start server
+app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server running on port ${process.env.PORT}`);
+});
